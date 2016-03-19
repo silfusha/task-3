2297289ac77b7e331638c0306b715c1e498980df
Lisitski Sergey <silfusha@gmail.com>
2016-03-19 18:29:11 +0300

    doned

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
     var video = document.querySelector('.camera__video'),
         canvas = document.querySelector('.camera__canvas');
 
+
     var getVideoStream = function (callback) {
         navigator.getUserMedia = navigator.getUserMedia ||
             navigator.webkitGetUserMedia ||
@@ -14,7 +15,9 @@
                     video.onloadedmetadata = function (e) {
                         video.play();
 
+
                         callback();
+
                     };
                 },
                 function (err) {
@@ -25,65 +28,68 @@
             console.log("getUserMedia not supported");
         }
     };
+    var filters = {
+        invert: function (pixel) {
+            pixel[0] = 255 - pixel[0];
+            pixel[1] = 255 - pixel[1];
+            pixel[2] = 255 - pixel[2];
 
-    var applyFilterToPixel = function (pixel) {
-        var filters = {
-            invert: function (pixel) {
-                pixel[0] = 255 - pixel[0];
-                pixel[1] = 255 - pixel[1];
-                pixel[2] = 255 - pixel[2];
+            return pixel;
+        },
+        grayscale: function (pixel) {
+            var r = pixel[0];
+            var g = pixel[1];
+            var b = pixel[2];
+            var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
 
-                return pixel;
-            },
-            grayscale: function (pixel) {
-                var r = pixel[0];
-                var g = pixel[1];
-                var b = pixel[2];
-                var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+            pixel[0] = pixel[1] = pixel[2] = v;
 
-                pixel[0] = pixel[1] = pixel[2] = v;
+            return pixel;
+        },
+        threshold: function (pixel) {
+            var r = pixel[0];
+            var g = pixel[1];
+            var b = pixel[2];
+            var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= 128) ? 255 : 0;
+            pixel[0] = pixel[1] = pixel[2] = v;
 
-                return pixel;
-            },
-            threshold: function (pixel) {
-                var r = pixel[0];
-                var g = pixel[1];
-                var b = pixel[2];
-                var v = (0.2126 * r + 0.7152 * g + 0.0722 * b >= 128) ? 255 : 0;
-                pixel[0] = pixel[1] = pixel[2] = v;
-
-                return pixel;
-            }
-        };
-
-        var filterName = document.querySelector('.controls__filter').value;
-
-        return filters[filterName](pixel);
-    };
-
-    var applyFilter = function () {
-        for (var x = 0; x < canvas.width; x++) {
-            for (var y = 0; y < canvas.height; y++) {
-                var pixel = canvas.getContext('2d').getImageData(x, y, 1, 1);
-
-                pixel.data = applyFilterToPixel(pixel.data);
-
-                canvas.getContext('2d').putImageData(pixel, x, y);
-            }
+            return pixel;
         }
+    };
+    var pixels;
+    var pixel = new Array(3);
+    var data;
+    var length;
+    var filter;
+    var filterName = document.querySelector('.controls__filter').value;
+    var applyFilter = function () {
+
+        pixels = canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+        length = pixels.data.length;
+        data = pixels.data;
+        filter = filters[filterName];
+        for (var i = 0; i < length; i += 4) {
+           pixel = filter( [data[i], data[i + 1], data[i + 1]]);
+           data[i] = pixel[0];
+           data[i + 1] = pixel[1];
+           data[i + 2] = pixel[2];
+
+        }
+        canvas.getContext('2d').putImageData(pixels, 0, 0);
+    };
+    document.querySelector('.controls__filter').onchange = function (event) {
+        filterName = event.srcElement.value;
     };
 
     var captureFrame = function () {
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-
-        canvas.getContext('2d').drawImage(video, 0, 0);
+        canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height);
         applyFilter();
+        requestAnimationFrame(captureFrame);
     };
 
     getVideoStream(function () {
+        canvas.width = video.videoWidth;
+        canvas.height = video.videoHeight;
         captureFrame();
-
-        setInterval(captureFrame, 16);
     });
 })();
